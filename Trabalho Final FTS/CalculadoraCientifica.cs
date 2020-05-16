7efefb7f3e8ab6380b29668f624f0cc878e4b7ca
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2020-05-16 20:09:59 -0300

    Criação da classe calculadora científica com seus respectivos métodos.

---
+++ Trabalho Final FTS/CalculadoraCientifica.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final_FTS
+{
+    public class CalculadoraCientifica : Calculadora
+    {
+        public static double Logaritmo(double a) //João
+        {
+            return Math.Log10(a);
+        }
+
+        public static double Sen(double a) //Lorena
+        {
+            return Math.Sin(a);
+        }
+
+        public static double Cos(double a) //Nathan
+        {
+            return Math.Cos(a);
+        }
+
+        public static double Tan(double a) //João
+        {
+            return Math.Tan(a);
+        }
+
+        public static double Radiciacao(double a) //Lorena
+        {
+            return Math.Sqrt(a);
+        }
+
+        public static double Potenciacao(double a,double b) //Nathan
+        {
+            return Math.Pow(a,b);
+        }
+
+        public static double Porcentagem(double a) //João
+        {
+            return (a / 100);
+        }
+
+        public static double Pi() //Lorena
+        {
+            return Math.PI;
+        }
+
+        public static double Ln(double a) //Nathan
+        {
+            return Math.Log(a);
+        }
+
+    }
+}
