83a71c5a18071a4f3ab6eaa08619a951b153a070
SenaGust <gustavosena98@live.com>
2020-05-16 14:49:03 -0300

    Configurações iniciais do projeto

---
+++ Trabalho Final FTS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final_FTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
