cacb24952fb7863f7f4a43271a70ac145bb55955
SenaGust <gustavosena98@live.com>
2020-05-16 15:16:21 -0300

    Criação das classes e implementação parcial dos métodos

---
+++ Trabalho Final FTS/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final_FTS
+{
+    public class Calculadora
+    {
+        public static double Somar(double A, double B)
+        {
+            return 0;
+        }
+        public static double Subtrair(double A, double B)
+        {
+            return 0;
+        }
+        public static double Multiplicar(double A, double B)
+        {
+            return 0;
+        }
+        public static double Dividir(double A, double B)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Trabalho Final FTS/CalculadoraTeste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Trabalho_Final_FTS
+{
+    public class CalculadoraTeste
+    {
+        [Fact]
+        public void FuncaoSomar()
+        {
+            
+        }
+        [Fact]
+        public void FuncaoSubtrair()
+        {
+
+        }
+        [Fact]
+        public void FuncaoMultiplicar()
+        {
+
+        }
+        [Fact]
+        public void FuncaoDividir()
+        {
+
+        }
+    }
+}
