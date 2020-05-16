8b19ae641d7ecb4fbaeb0506bea4a3fec54bd913
Nathan Pinto <nathan.pinto@bhs.com.br>
2020-05-16 17:17:17 -0300

    Testes em projeto separado

---
+++ Trabalho Final FTSTest/CalculadoraUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trabalho_Final_FTS;
+using Xunit;
+
+namespace Trabalho_Final_FTSTest
+{
+    public class CalculadoraUnitTest
+    {
+        [Fact]
+        public void Somar_DoisDouble_RetornaDouble()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var valorEsperado = 6;
+            // Act  
+            var soma = Calculadora.Somar(num1, num2);
+            //Assert  
+            Assert.Equal(valorEsperado, soma);
+        }
+        [Fact]
+        public void Subtrair_DoisDouble_RetornaDouble()
+        {
+
+        }
+        [Fact]
+        public void Multiplicar_DoisDouble_RetornaDouble()
+        {
+
+        }
+        [Fact]
+        public void Dividir_DoisDouble_RetornaDouble()
+        {
+            Assert.Equal(4, Calculadora.Dividir(8, 2));
+        }
+        [Fact]
+        public void Dividir_DivisaoPor0_RetornaErro()
+        {
+            Assert.Throws<DivideByZeroException>(() => Calculadora.Dividir(1, 0));
+        }
+    }
+}
