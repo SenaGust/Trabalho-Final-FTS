f203212199970ad07069ab850eaa9780477b6544
Nathan Pinto <nathan.pinto@bhs.com.br>
2020-05-16 22:06:50 -0300

    Correção de nomes de métodos

--- Trabalho Final FTSTest/CalculadoraUnitTest.cs
+++ Trabalho Final FTSTest/CalculadoraUnitTest.cs
@@ -10,7 +10,7 @@ namespace Trabalho_Final_FTSTest
     {
         [Fact]
         [Trait("Calculadora", "Somar")]
-        public void Somar_DoisDoublePositivos_RetornaInt()
+        public void Somar_DoisDoublePositivos_RetornaDouble()
         {
             // Arrange
             double num1 = 2.9;
@@ -23,7 +23,7 @@ namespace Trabalho_Final_FTSTest
         }
         [Fact]
         [Trait("Calculadora", "Somar")]
-        public void Somar_DoisDoubleNegativos_RetornaInt()
+        public void Somar_DoisDoubleNegativos_RetornaDouble()
         {
             // Arrange
             double num1 = -2.9;
@@ -36,7 +36,7 @@ namespace Trabalho_Final_FTSTest
         }
         [Fact]
         [Trait("Calculadora", "Somar")]
-        public void Somar_PositivoNegativo_RetornaInt()
+        public void Somar_PositivoNegativo_RetornaDouble()
         {
             // Arrange
             double num1 = 2.9;
