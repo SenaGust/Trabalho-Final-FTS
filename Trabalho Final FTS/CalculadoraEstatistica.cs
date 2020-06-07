d545bbe9130ab1cb00ede99132aa075b9948a520
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2020-06-07 20:35:01 -0300

    Criacao da classe CalculadoraEstatistica com seus metodos nao implementados e da sua classe de testes com os metdos tambem nao implementados.

---
+++ Trabalho Final FTS/CalculadoraEstatistica.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final_FTS
+{
+    public class CalculadoraEstatistica : Calculadora
+    {
+        public static double Media(double [] conjuntoNumeros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Minimo(double[] conjuntoNumeros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Maximo(double[] conjuntoNumeros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Mediana(double[] conjuntoNumeros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double DesvioPadrao(double[] conjuntoNumeros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Variancia(double[] conjuntoNumeros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Trabalho Final FTSTest/CalculadoraEstatisticaUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Trabalho_Final_FTSTest
+{
+    //Faz o teste falhar (fazer todo o teste e fazer o push)
+    //Faz o suficiente para passar (fazer somente o código passar e faz um push)
+    //Refatora (fazer a refotaração e fazer o push)
+
+    public class CalculadoraEstatisticaUnitTest
+    {
+        [Fact]
+        [Trait("CalculadoraEstatistica", "Media")] //Gustavo
+        public void Media_ConjuntoNumerosInteiros_RetornaDouble()
+        {
+            Assert.Equal(0, 1);
+        }
+
+        [Fact]
+        [Trait("CalculadoraEstatistica", "Minimo")] //João
+        public void Minimo_ConjuntoNumerosInteiros_RetornaDouble()
+        {
+            Assert.Equal(0, 1);
+        }
+
+        [Fact]
+        [Trait("CalculadoraEstatistica", "Maximo")] //Lorena
+        public void Maximo_ConjuntoNumerosInteiros_RetornaDouble()
+        {
+            Assert.Equal(0, 1);
+        }
+
+        [Fact]
+        [Trait("CalculadoraEstatistica", "Mediana")] //Nathan
+        public void Mediana_ConjuntoNumerosInteiros_RetornaDouble()
+        {
+            Assert.Equal(0, 1);
+        }
+
+        [Fact]
+        [Trait("CalculadoraEstatistica", "DesvioPadrão")] //Gustavo
+        public void DesvioPadrao_ConjuntoNumerosInteiros_RetornaDouble()
+        {
+            Assert.Equal(0, 1);
+        }
+
+        [Fact]
+        [Trait("CalculadoraEstatistica", "Variância")] //João
+        public void Variancia_ConjuntoNumerosInteiros_RetornaDouble()
+        {
+            Assert.Equal(0, 1);
+        }
+    }
+}
